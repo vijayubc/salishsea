a9b6cd91a3bf3f8a284de3fb96897af55d98c438
Karsten Bolding <bolding@bios.au.dk>
2017-09-13 14:27:33 -0700

    added ncks commands to generate_topo.sh

--- SSMSP/generate_topo.sh
+++ SSMSP/generate_topo.sh
@@ -5,7 +5,12 @@
 
 res=0.0300x0.020
 cdo remapbil,grid_$res nemo.nc topo_$res.nc
+#ncks -d lon,10,144 -d lat,14,208 topo_$res.nc topo_2km.nc
+
 res=0.0150x0.010
 cdo remapbil,grid_$res nemo.nc topo_$res.nc
+#ncks -d lon,22,290 -d lat,22,415 topo_$res.nc topo_1m.nc
+
 res=0.0075x0.005
 cdo remapbil,grid_$res nemo.nc topo_$res.nc
+#ncks -d lon,42,576 -d lat,50,829 topo_$res.nc topo_500m.nc
