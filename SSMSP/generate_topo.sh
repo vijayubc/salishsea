07359cff86aba0835c3992328c34b0fa0af0d86e
Karsten Bolding <karsten@bolding-bruggeman.com>
2017-09-14 13:22:56 -0700

    added additional configuration in generate_topo.sh

--- SSMSP/generate_topo.sh
+++ SSMSP/generate_topo.sh
@@ -6,11 +6,17 @@
 res=0.0300x0.020
 cdo remapbil,grid_$res nemo.nc topo_$res.nc
 #ncks -d lon,10,144 -d lat,14,208 topo_$res.nc topo_2km.nc
+#ncatted -a missing_value,bathymetry,m,f,-10. topo_2km.nc
+#ncatted -a _FillValue,bathymetry,m,f,-10. topo_2km.nc
 
 res=0.0150x0.010
 cdo remapbil,grid_$res nemo.nc topo_$res.nc
-#ncks -d lon,22,290 -d lat,22,415 topo_$res.nc topo_1m.nc
+#ncks -d lon,22,290 -d lat,22,415 topo_$res.nc topo_1km.nc
+#ncatted -a missing_value,bathymetry,m,f,-10. topo_1km.nc
+#ncatted -a _FillValue,bathymetry,m,f,-10. topo_1km.nc
 
 res=0.0075x0.005
 cdo remapbil,grid_$res nemo.nc topo_$res.nc
 #ncks -d lon,42,576 -d lat,50,829 topo_$res.nc topo_500m.nc
+#ncatted -a missing_value,bathymetry,m,f,-10. topo_500m.nc
+#ncatted -a _FillValue,bathymetry,m,f,-10. topo_500m.nc
