4fce70b352738d1861a74875ef00ba93d6eb032a
Karsten Bolding <karsten@bolding-bruggeman.com>
2018-01-26 09:58:32 +0100

    2km: add run_tides.sh

---
+++ setups/salish_2km/run_tides.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+exedir=~/local/intel/17.0.3/getm/bin
+exedir=~/local/gnu/5/getm/bin
+
+# run spin-up
+make namelist runtype=1 hotstart=False start="2015-12-01 00:00:00" stop="2016-01-01 00:00:00"
+mpiexec -np 8 $exedir/getm_spherical_parallel
+mv salish_2km.2d.????.nc salish_2km.????.stderr HOT/
+
+#mmv 'restart*.out' 'restart#1.in'
+rename 's/\.out$/\.in/' restart.????.out
+
+# run annual tidal simulation
+make namelist runtype=1 hotstart=True start="2016-01-01 00:00:00" stop="2017-01-01 00:00:00"
+mpiexec -np 8 $exedir/getm_spherical_parallel
+
+# merge result files
+ncmerge salish_2km.2d.????.nc salish_2km.2d.nc
